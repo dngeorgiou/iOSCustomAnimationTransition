6d4cd55b3823fd5ec5ffa3e7592fb8d9a0f03e6f
damongeorgiou <dngx113@gmail.com>
2019-01-19 15:38:27 -0500

    implement CustomAnimator

--- CustomNavigationAnimations-Starter/MusicPickViewController.swift
+++ CustomNavigationAnimations-Starter/MusicPickViewController.swift
@@ -8,16 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UINavigationControllerDelegate {
 
     @IBOutlet weak var collectionView: UICollectionView!
     private let cellId = "MusicCell"
     private var selectedSong : Song?
+    private var selectedFrame: CGRect?
     
     private var songs : [Song] = [Song(artist: "Kendrick Lamar", title: "DNA", artwork: UIImage(named: "damn")!),Song(artist: "Freddie Gibbs", title: "Alexys", artwork: UIImage(named: "twice")!), Song(artist: "Brockhampton", title: "JUNKY", artwork: UIImage(named: "sat")!), Song(artist: "MC Eiht", title: "Represent Like This", artwork: UIImage(named: "west")!), Song(artist: "Tyler the Creator", title: "November", artwork: UIImage(named: "flower")!), Song(artist: "Jay-Z", title: "4:44", artwork: UIImage(named: "four")!), Song(artist: "Joey Bada$$", title: "ROCKABYE BABY", artwork: UIImage(named: "badass")!), Song(artist: "Sean Price", title: "Imperious Rex", artwork: UIImage(named: "imperious")!), Song(artist: "Oddisee", title: "Hold it back", artwork: UIImage(named: "iceburg")!), Song(artist: "J.I.D", title: "Never", artwork: UIImage(named: "never")!)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.delegate = self
+        
         songs.append(contentsOf: songs + songs + songs)
         SetupCollectionView()
     }
@@ -44,7 +47,21 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         self.selectedSong = songs[indexPath.row]
+        let theAttributes:UICollectionViewLayoutAttributes! = collectionView.layoutAttributesForItem(at: indexPath)
+        selectedFrame = collectionView.convert(theAttributes.frame, to: collectionView.superview)
         self.performSegue(withIdentifier: "Player", sender: self)
+    }
+    
+    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationControllerOperation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        guard let frame = self.selectedFrame else { return nil }
+        guard let song = self.selectedSong else { return nil }
+        
+        switch operation {
+        case .push:
+            return CustomAnimator(duration: TimeInterval(UINavigationControllerHideShowBarDuration), isPresenting: true, originFrame: frame, image: song.artwork)
+        default:
+            return CustomAnimator(duration: TimeInterval(UINavigationControllerHideShowBarDuration), isPresenting: false, originFrame: frame, image: song.artwork)
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
